b6e819f51c0ae53c00c8a30fbef120d321f9f539
Mr. Dragon <whyorean@gmail.com>
2019-06-09 02:01:30 +0530

    Repository : Autoscroll logs

--- app/src/main/java/com/aurora/adroid/fragment/RepoFragment.java
+++ app/src/main/java/com/aurora/adroid/fragment/RepoFragment.java
@@ -21,6 +21,7 @@ package com.aurora.adroid.fragment;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -140,6 +141,7 @@ public class RepoFragment extends Fragment {
                         .commitAllowingStateLoss();
         });
         btnSync.setOnClickListener(v -> syncDatabase());
+        txtLog.setMovementMethod(new ScrollingMovementMethod());
     }
 
     private void syncDatabase() {
