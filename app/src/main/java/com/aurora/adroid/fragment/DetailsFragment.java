a83809bc90dd10d3690cae69e4958187c73a8c2e
Mr. Dragon <whyorean@gmail.com>
2019-07-18 00:50:03 +0530

    Details : Refresh package list post install/uninstall

--- app/src/main/java/com/aurora/adroid/fragment/DetailsFragment.java
+++ app/src/main/java/com/aurora/adroid/fragment/DetailsFragment.java
@@ -65,6 +65,7 @@ public class DetailsFragment extends Fragment {
     private CompositeDisposable disposable = new CompositeDisposable();
     private DetailsInstallReceiver detailsInstallReceiver;
     private AppActionDetails appActionDetails;
+    private AppPackages appPackages;
 
     @Override
     public void onAttach(@NonNull Context context) {
@@ -164,12 +165,13 @@ public class DetailsFragment extends Fragment {
         new AppSubInfoDetails(this, app).draw();
         new AppLinkDetails(this, app).draw();
         new AppScreenshotsDetails(this, app).draw();
-        new AppPackages(this, app).draw();
     }
 
     public void drawButtons() {
         appActionDetails = new AppActionDetails(this, app);
+        appPackages = new AppPackages(this, app);
         appActionDetails.draw();
+        appPackages.draw();
     }
 
     private void notifyAction(String message) {
