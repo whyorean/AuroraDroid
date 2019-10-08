3c80d8b74725d4ecc0852154411e76bb92c11ef4
Rahul Kumar Patel <whyorean@gmail.com>
2019-10-08 21:57:41 +0530

    Downloader : Remove cookies

--- app/src/main/java/com/aurora/adroid/download/DownloadManager.java
+++ app/src/main/java/com/aurora/adroid/download/DownloadManager.java
@@ -26,15 +26,6 @@ import com.tonyodev.fetch2.Fetch;
 import com.tonyodev.fetch2.FetchConfiguration;
 import com.tonyodev.fetch2okhttp.OkHttpDownloader;
 
-import org.jetbrains.annotations.NotNull;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-
-import okhttp3.Cookie;
-import okhttp3.CookieJar;
-import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 
 public class DownloadManager {
@@ -76,22 +67,7 @@ public class DownloadManager {
     }
 
     private static OkHttpClient getOkHttpClient(Context context) {
-        OkHttpClient.Builder builder = new OkHttpClient.Builder()
-                .cookieJar(new CookieJar() {
-                    private final HashMap<HttpUrl, List<Cookie>> cookieStore = new HashMap<>();
-
-                    @Override
-                    public void saveFromResponse(@NotNull HttpUrl url, @NotNull List<Cookie> cookies) {
-                        cookieStore.put(url, cookies);
-                    }
-
-                    @NotNull
-                    @Override
-                    public List<Cookie> loadForRequest(@NotNull HttpUrl url) {
-                        List<Cookie> cookies = cookieStore.get(url);
-                        return cookies != null ? cookies : new ArrayList<>();
-                    }
-                });
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
         if (Util.isNetworkProxyEnabled(context))
             builder.proxy(Util.getNetworkProxy(context));
         return builder.build();
