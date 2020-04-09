db06d9dae8b3578f19347ff5c860cbde8c4c97a9
Rahul Kumar Patel <whyorean@gmail.com>
2020-04-09 16:55:40 +0530

    Code Maid [1/2]

--- app/src/main/java/com/aurora/adroid/util/CertUtil.java
+++ app/src/main/java/com/aurora/adroid/util/CertUtil.java
@@ -1,13 +1,10 @@
 /*
  * Aurora Droid
- * Copyright (C) 2019, Rahul Kumar Patel <whyorean@gmail.com>
- *
- * ChecKey - Guardian Project
- * Copyright (C) 2018 Hans-Christoph Steiner
+ * Copyright (C) 2019-20, Rahul Kumar Patel <whyorean@gmail.com>
  *
  * Aurora Droid is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
+ * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * Aurora Droid is distributed in the hope that it will be useful,
@@ -16,8 +13,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Aurora Store.  If not, see <http://www.gnu.org/licenses/>.
- *
+ * along with Aurora Droid.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
 
